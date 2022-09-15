7575263047e993eee571268566e1c85c6e37b752
kuifir <kuifir@163.com>
2022-09-15 22:42:28 +0800

    枚举类的注入，Spring Resource的注入

--- ioc-container-overview/src/main/java/kuifir/ioc/overview/domain/User.java
+++ ioc-container-overview/src/main/java/kuifir/ioc/overview/domain/User.java
@@ -1,5 +1,8 @@
 package kuifir.ioc.overview.domain;
 
+import kuifir.ioc.overview.enums.City;
+import org.springframework.core.io.Resource;
+
 /**
  * Package: kuifir.ioc.overview.domain
  * <p>
@@ -14,6 +17,27 @@ package kuifir.ioc.overview.domain;
 public class User {
     private Long id;
     private String name;
+    private City city;
+
+    private Resource configFileLocation;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", city=" + city +
+                ", configFileLocation=" + configFileLocation +
+                '}';
+    }
+
+    public Resource getConfigFileLocation() {
+        return configFileLocation;
+    }
+
+    public void setConfigFileLocation(Resource configFileLocation) {
+        this.configFileLocation = configFileLocation;
+    }
 
     public Long getId() {
         return id;
@@ -31,13 +55,14 @@ public class User {
         this.name = name;
     }
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
+    public City getCity() {
+        return city;
     }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
     public static User createUser() {
         User user = new User();
         user.setName("kuifir");
---
+++ ioc-container-overview/src/main/java/kuifir/ioc/overview/enums/City.java
@@ -0,0 +1,16 @@
+package kuifir.ioc.overview.enums;
+
+/**
+ * Package: kuifir.ioc.overview
+ * <p>
+ * Description： TODO
+ * <p>
+ * Author: baci
+ * <p>
+ * Date: Created in 2022/9/15 22:27
+ * <p>
+ * Version: 0.0.1
+ */
+public enum City {
+    BEIJING,HANGZHOU,SHANGHAI
+}
